9a226f2bba7da805a40662a5af441d7b9b75215d
new-happy <309615655@qq.com>
2017-11-23 14:22:17 +0800

    1

--- js/TeamDetails.js
+++ js/TeamDetails.js
@@ -2,19 +2,19 @@
       window.location.href="orderTime.html"; 
 }) 
     
-		$('.top-up>p').tap(function(){
-			console.log($(this).index())
-			if($(this).index() ===0){
+		$('.top-up>p>a').tap(function(){
+			console.log($(this).parent().index())
+			if($(this).parent().index() ===0){
 				var top1 = $('.repeat').offset().top
-				$('html').scrollTop(top1)
+				$('html,body').scrollTop(top1)
 			}
-			if($(this).index() ===1){
+			if($(this).parent().index() ===1){
 				var top2 = $('.repeat').eq(1).offset().top
-				$('html').scrollTop(top2)
+				$('html,body').scrollTop(top2)
 			}
-			if($(this).index() ===2){
+			if($(this).parent().index() ===2){
 				var top3 = $('.repeat').eq(2).offset().top
-				$('html').scrollTop(top3)
+				$('html,body').scrollTop(top3)
 			}
 		})
 		$('.lingdui').tap(function(){
